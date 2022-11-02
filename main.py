5c0bfd3d20745a2b9e8d3867170e003a13685327
Kevin Santiago <113930456+ksantiago772@users.noreply.github.com>
2022-11-02 17:47:47 +0000

    Made our word bank

---
+++ main.py
@@ -0,0 +1,4 @@
+import random 
+word list = ("august, airplane, aviation, airpods, arbitrary,bedroom, banana, bicycle, beauty iphone, computer, comcast, pumpkin, evins, holiday, music, family, school, elect, faster, earth, eager, solid, sand, stupid, scar,virus")
+word = random.choice(word_list)
+
